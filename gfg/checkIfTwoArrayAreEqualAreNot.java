7a69a3d487d22aec15252bc8a65ead61a4f06e2a
Shivsaran7776 <shivisaran7776@gmail.com>
2023-06-20 05:49:34 +0530

    Commit Successful

---
+++ gfg/checkIfTwoArrayAreEqualAreNot.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+class TwoArraysAreEqualOrNot
+{
+    public static boolean check(int A[],int B[],int N)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int count = 0;
+        for(int i=0;i<N;i++)
+        {
+            if (A[i]==B[i])
+            {
+                count++;
+            }
+        }
+        return(count==N);
+    }
+}
+
+public class checkIfTwoArrayAreEqualAreNot {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int times;
+        System.out.println("Enter the no.of times loop to be iterated:");
+        times = sc.nextInt();
+        while(times>0)
+        {
+            int size;
+            System.out.println("Enter the size of the array:");
+            size = sc.nextInt();
+            int arr[] = new int[size]; 
+            int brr[] = new int[size];
+            System.out.println("Enter first array elements:");
+            for(int i=0;i<size;i++)
+            {
+
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("Enter second array elements:");
+            for(int i=0;i<size;i++)
+            {
+
+                brr[i] = sc.nextInt();
+            }
+            TwoArraysAreEqualOrNot obj = new TwoArraysAreEqualOrNot();
+            System.out.println("Result of two array after check equal or not:");
+            System.out.println(obj.check(arr, brr, size)==true?"1":"0");
+            times--;
+        }
+        sc.close();
+    }
+}
