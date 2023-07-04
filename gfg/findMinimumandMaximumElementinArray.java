b9d69f7cfa7e872705776775c76f572230f3c8b8
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-04 06:55:14 +0530

    Commit Successful

---
+++ gfg/findMinimumandMaximumElementinArray.java
@@ -0,0 +1,10 @@
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+        Arrays.sort(a);
+        pair data = new pair(a[0],a[(int)n-1]);
+        return data;
+    }
+}
