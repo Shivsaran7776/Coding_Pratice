7624ce44687522bb381b8873d621da1cf2921118
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-05 06:54:40 +0530

    Commit Successful

---
+++ gfg/BiotonicPoint.java
@@ -0,0 +1,12 @@
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        int max = arr[0];
+        for (int i=0;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+       
+    }
+}
