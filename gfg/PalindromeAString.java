a4aa480c024eb531145b81306c55b3000ba86a06
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-06 07:15:48 +0530

    Commit Successful

---
+++ gfg/PalindromeAString.java
@@ -0,0 +1,19 @@
+class Solution {
+    int isPalindrome(String S) {
+        // code here
+        String reverse="";
+        int length = S.length();
+        for(int i=length-1;i>=0;i--)
+        {
+            reverse = reverse+S.charAt(i);   
+        }
+        if(S.equals(reverse))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
