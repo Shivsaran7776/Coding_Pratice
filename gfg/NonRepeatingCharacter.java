1a9bb6e692303ff53bcd90f7f506fcc0530f87ce
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-06 07:14:08 +0530

    Commit Successful

---
+++ gfg/NonRepeatingCharacter.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+        int count =1;
+        for(int i=0;i<S.length();i++)
+        {
+            if(S.charAt(i)==S.charAt(i+1))
+            {
+                continue;
+            }
+            else
+            {
+                return (S.charAt(i));
+            }
+        }
+        return '$';
+    }
+}
