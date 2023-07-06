1fab561f0b8d08803598df7611dc63253c8df97c
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-06 07:13:01 +0530

    Commit Successful

---
+++ gfg/MissingNumberInArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    int MissingNumber(int array[], int n) {
+        Arrays.sort(array);
+        int r = n*(n+1)/2;
+        int tot = 0;
+        for(int i=0;i<n-1;i++){
+            tot = tot+array[i];
+        }
+        if(tot!=r){
+            int result = r-tot;
+            return result;
+        }
+        return 0;
+        }
+    }
