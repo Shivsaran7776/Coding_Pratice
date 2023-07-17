64a0a99ceadd173462f8f99fa9dcd4b67a0ca110
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-17 06:09:05 +0530

    Commit Successful

---
+++ gfg/Pattern/Pattern-7.java
@@ -0,0 +1,32 @@
+Example 1:
+
+Input: 5
+
+Output:
+    *
+   ***  
+  *****
+ *******
+*********
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i =0;i<n;i++)
+        {
+            for(int j=1;j<=n+i;j++)
+            {
+                if(j<n-i)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
