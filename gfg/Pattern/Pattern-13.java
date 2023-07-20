2dbb18afb9015df89b3b63092e479079a7fd7f22
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-20 08:04:37 +0530

    Commit Successful

---
+++ gfg/Pattern/Pattern-13.java
@@ -0,0 +1,28 @@
+Example 1:
+
+Input: 5
+
+Output:
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+class Solution {
+
+    void printTriangle(int n) 
+    {
+        int num = 0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                
+                num +=1;
+                System.out.print(num+" ");
+            }
+            System.out.println();
+        }
+    }
+}
