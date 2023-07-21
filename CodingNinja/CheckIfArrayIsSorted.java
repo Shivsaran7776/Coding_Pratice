84280d9e1fbcc5881054dcc1546155820a9118f4
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-21 22:09:36 +0530

    Commit Successful

---
+++ CodingNinja/CheckIfArrayIsSorted.java
@@ -0,0 +1,19 @@
+Sample Input 1 :
+4
+0 0 0 1
+Sample Output 1 :
+1
+
+public class Solution {
+    public static int isSorted(int n, int []a) {
+        // Write your code here.
+        for(int i=1;i<n;i++)
+        {
+            if(a[i-1]>a[i])
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
