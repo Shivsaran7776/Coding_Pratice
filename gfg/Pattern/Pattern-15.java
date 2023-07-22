66754ab72c86c72f971a1b96484b15b144fa76db
Shivsaran7776 <shivisaran7776@gmail.com>
2023-07-22 23:04:11 +0530

    Commit Successful

---
+++ gfg/Pattern/Pattern-15.java
@@ -0,0 +1,25 @@
+Example 1:
+
+Input: 5
+
+Output:
+ABCDE
+ABCD
+ABC
+AB
+A
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print((char)(65+j));
+            }
+            System.out.println();
+        }
+    }
+}
