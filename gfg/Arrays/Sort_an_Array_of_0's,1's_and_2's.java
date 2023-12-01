4c5b21ed21434a197f124afa25fa00f4436f37d8
Shivsaran7776 <shivisaran7776@gmail.com>
2023-12-01 07:17:12 +0530

    Commit Successful

--- gfg/Arrays/Sort_an_Array_of_0's,1's_and_2's.java
+++ gfg/Arrays/Sort_an_Array_of_0's,1's_and_2's.java
@@ -17,3 +17,47 @@ class Solution
         
     }
 }
+                                                             (or)
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i] == 0)
+            {
+                zeros++;
+            }
+            else if(a[i] == 1)
+            {
+                ones++;
+            }
+            else
+            {
+                twos++;
+            }
+        }
+        int i = 0;
+        while(i<n)
+        {
+            if(i < zeros)
+            {
+                a[i++] = 0;
+            }
+            else if(i < zeros+ones)
+            {
+                a[i++] = 1;
+            }
+            else if(i < zeros+ones+twos)
+            {
+                a[i++] = 2;
+            }
+            
+        }
+    }
+}                                                            
